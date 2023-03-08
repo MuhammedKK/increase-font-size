e66f693e2ef40b499d61b78c8a0f74e4e17104f9
Muhammed Khalid. Saad <mkhalid@Informatique.net>
2023-03-08 12:50:18 +0200

    Uploaded Files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+
+  const font_plus = () => {
+    var elements = document.querySelectorAll(" h1, h2, h3, h4, h5, h6, p, li, a, b, th, tr, td, span");
+    var fs;
+    for(let i = 0; i<elements.length; i++){
+      fs = window.getComputedStyle(elements[i]).fontSize.replace('px', '');
+      fs ++;
+      // if(a[i].style.fontSize){
+        elements[i].style.fontSize = fs + 'px' 
+      // }
+    }
+  }
+  const font_minus = () => {
+    var a = document.querySelectorAll(" h1, h2, h3, h4, h5, h6, p, li, a, b, th, tr, td, span ");
+    var fs;
+    for(let i = 0; i<a.length; i++){
+      fs = window.getComputedStyle(a[i]).fontSize.replace('px', '');
+      fs --;
+      a[i].style.fontSize = fs + 'px' 
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <React.Fragment>
+      <div className='App'>
+        <button onClick={font_plus}>Click To Increase</button>
+        <h1>Hey Man Hey</h1>
+        <h2>Hey Man Hey</h2>
+        <h3>Hey Man Hey</h3>
+        <p>Hey Man Hey</p>
+        <span>Hey Man Hey</span>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          mohamed khaled saad
+          
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <button onClick={font_minus}>Click To Decrease</button>
+      </div>
+    </React.Fragment>
   );
 }
 
